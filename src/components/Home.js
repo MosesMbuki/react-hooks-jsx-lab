6d8449f86552dfcaab623e84bf00224276f7b76d
+ <moses.mutitu@student.moringaschool.com>
2025-04-15 17:47:32 +0300

    complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const divstyle = {
+    color: "firebrick",   
+  };
+  return (
+    <div id="home">Home
+    <h1 style={divstyle}>
+      {name} is a Web Developer from {city}
+    </h1>
+  </div>);
 }
 
 export default Home;
